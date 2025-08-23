dc1d50a79708bb28991846e0defb6a86dfc1945e
samokander <andrei.samokish111@gmail.com>
2025-08-24 00:40:52 +0300

    update: 0.8.29 version

--- src/new/interfaces/ILendingPool.sol
+++ src/new/interfaces/ILendingPool.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: SEE LICENSE IN LICENSE
-pragma solidity 0.8.30;
+pragma solidity 0.8.29;
 
 // Aave V2 lending pool minimal interface
 interface ILendingPool {
--- src/new/interfaces/IUniswapV2Router02.sol
+++ src/new/interfaces/IUniswapV2Router02.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: SEE LICENSE IN LICENSE
-pragma solidity 0.8.30;
+pragma solidity 0.8.29;
 
 interface IUniswapV2Router02 {
     function swapExactTokensForTokens(
