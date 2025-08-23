54dda7c574b44cd9202a2805491b813e90aa68da
samokander <andrei.samokish111@gmail.com>
2025-08-23 23:42:51 +0300

    chore: interfaces refactor

---
+++ src/new/interfaces/IFlashLoanReceiver.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity 0.8.30;
+
+// Aave V2 receiver interface
+interface IFlashLoanReceiver {
+    function executeOperation(
+        address[] calldata assets,
+        uint256[] calldata amounts,
+        uint256[] calldata premiums,
+        address initiator,
+        bytes calldata params
+    ) external returns (bool);
+}
---
+++ src/new/interfaces/ILendingPool.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity 0.8.30;
+
+// Aave V2 lending pool minimal interface
+interface ILendingPool {
+    function flashLoan(
+        address receiverAddress,
+        address[] calldata assets,
+        uint256[] calldata amounts,
+        uint256[] calldata modes,
+        address onBehalfOf,
+        bytes calldata params,
+        uint16 referralCode
+    ) external;
+}
